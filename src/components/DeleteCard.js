9ea50307ff23cc6576beade4ffc0a4fafecec1fd
James Bond <57707960+rcavalero@users.noreply.github.com>
2020-02-22 15:46:18 -0800

    include delete indredient and recommendation functionality

--- src/components/DeleteCard.js
+++ src/components/DeleteCard.js
@@ -1,49 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import API from "../utils/API"
 import './styles/Delete.css';
 
 function DeleteCard(props) {
-    
-    function deleteIngredient() {
-        props.handleDeleteIngredient(props.ingredient.id);
-    }
-
-    function deleteRecommendation() {
-        props.handleDeleteRecommendation(props.recommendation.id);
-    }
-
     return (
 
         <div className="container">
-            
+
             <h2>Delete Ingredients / Recommendations</h2>
             <div className="row inputbox">
-
                 <div className="input-field col s12">
-
-                    <select className="browser-default" defaultValue={"DEFAULT"} onChange={props.handleIngredientSelection}>
-            
-                    <button onClick={deleteIngredient} ><i className="far fa-trash-alt fa-2x"></i></button>
-            
-                        {/* <option value="DEFAULT" disabled>Select an Ingredient</option>
-            
+                    <select className="browser-default" defaultValue={""} onChange={props.handleDeleteIngredientSelection}>
+                        <option value="" disabled>Select an Ingredient</option>
                         {props.ingredients.map(ing => (
-                            <option value={ing.id} key={ing.id}>{ing.name}</option>
-                        ))} */}
+                            <option value={ing.id} key={ing.id} name="delIngName" >{ing.name} </option>
+                        ))}
                     </select>
+                    <button onClick={props.handleDeleteIngredient} ><i className="far fa-trash-alt fa-2x"></i></button>
                 </div>
             </div>
 
-            {/* <div className="input-field col s12">
-                <ul className="list-group">
-                    <li className="list-group-item" >
-                        <h5>{props.recommendations.brand}</h5>
-                        <button onClick={deleteRecommendation} ><i className="far fa-trash-alt fa-2x"></i></button>
-                    </li>
-                </ul>
-            </div> */}
-        </div>        
+            <div>
+                {props.recommendations.length > 0 ? (
+
+                    <div className="input-field col s12">
+                        <ul className="list-group">
+                            {props.recommendations.map(rec => (
+                                <li className="list-group-item" value={rec.id} key={rec.id} name="recommendation">
+                                    {rec.brand} 
+                                    
+                                        <button value={rec.id} onClick={props.handleDeleteRecommendation}><span value={rec.id} onClick={props.handleDeleteRecommendation}><i className="far fa-trash-alt fa-2x" value={rec.id}></i></span></button>
+                                            
+                                </li>))}
+                        </ul>
+                    </div>
+                    // </div>
+                ) : <p>Select Ingredient to see Recommendations</p>}
+            </div>
+        </div>
     )
 }
-            
+
 export default DeleteCard
